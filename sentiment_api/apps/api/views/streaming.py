b48a1ad088e39c94ea35e12200baee53cc77c4f9
Joseph Moussa <joeph465@gmail.com>
2021-12-12 10:33:42 -0500

    websocket api endpoint that starts a pulsar consumer

--- sentiment_api/apps/api/views/streaming.py
+++ sentiment_api/apps/api/views/streaming.py
@@ -1,14 +1,71 @@
 # -*- coding: utf-8 -*-
+from typing import List
 
 import logging
-from fastapi import WebSocket
+import json
+from fastapi import WebSocket, WebSocketDisconnect
 from fastapi.routing import APIRouter
 from ..models.tweet import TweetRequest
 from utils import execute_search, get_trending_tweets
+import pulsar
+from tweet_analysis.config import config
+
 
 router = APIRouter()
 
 logger = logging.getLogger(__name__)
+
+
+class ConnectionManager:
+    def __init__(self):
+        self.active_connections: List[WebSocket] = []
+
+    async def connect(self, websocket: WebSocket):
+        await websocket.accept()
+        self.active_connections.append(websocket)
+
+    def disconnect(self, websocket: WebSocket):
+        self.active_connections.remove(websocket)
+
+    async def send_personal_message(self, message: str, websocket: WebSocket):
+        await websocket.send_text(message)
+
+    async def broadcast(self, message: str):
+        for connection in self.active_connections:
+            await connection.send_text(message)
+
+
+manager = ConnectionManager()
+
+
+@router.websocket("/ws/{topic}/{subscription}")
+async def websocket_endpoint(websocket: WebSocket, topic: str, subscription: str):
+    """
+    Websocket connection to stream-in tweets.
+    Under the hood: Start up pulsar consumer to pull in tweets based on the search query
+
+    """
+    # TODO: Abstract this to config
+    client = pulsar.Client(config.pulsar_client_url)
+    await manager.connect(websocket)
+    try:
+        consumer = client.subscribe(topic, subscription)
+        while True:
+            data = await websocket.receive_text()
+            logger.info(f"WS Connected, {data}\nInitializing Pulsar Stream")
+            # Start Pulsar consumer and consume messages back to API
+            msg = consumer.receive()
+            try:
+                logger.info("Received message '{}' id='{}'".format(msg.data(), msg.message_id()))
+                # Acknowledge successful processing of the message
+                await manager.send_personal_message(msg.data(), websocket)
+                consumer.acknowledge(msg)
+            except:
+                # Message failed to be processed
+                consumer.negative_acknowledge(msg)
+    except WebSocketDisconnect:
+        client.close()
+        manager.disconnect(websocket)
 
 
 @router.get("/tweets")
@@ -21,9 +78,7 @@ async def fetch_tweets(req: TweetRequest):
             logger.info("Returning Most Recent Content")
             # most recent scored and sorted
             query = {
-                "query": {
-                    "bool": {"must": {"exists": {"field": "analysis_nltk_compound"}}}
-                },
+                "query": {"bool": {"must": {"exists": {"field": "analysis_nltk_compound"}}}},
                 "sort": [{"created_at": "desc"}],
             }
             docs = execute_search(query, size=req.limit)
